264ec9706747917e87b41bce996218595b2e1b29
SaiAdityya <saiadityareddysirigiri@gmail.com>
2026-09-18 23:00:52 +0530

    Time: 2 ms (99.8%), Space: 43.4 MB (83.6%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,34 +1,22 @@
-import java.util.*;
 class Solution {
-    public static String removeOuterParentheses(String s) {
-        Deque<Character> ans = new ArrayDeque<>();
-        StringBuilder s_ans = new StringBuilder();
-        char left = '(';
-        char right = ')';
-        int left_count = 0;
-        int right_count = 0;
-        for(int i = s.length() - 1;i >= 0;i--){
-            char temp = s.charAt(i);
-            if(temp == right){
-                right_count++;
-                ans.push(temp);
-            }
-            if(temp == left){
-                left_count++;
-                ans.push(temp);
-            }
-            if(left_count == right_count && ans.peek() == left){
-                int temp1 = ans.pop();
-                StringBuilder temp_string = new StringBuilder();
-                while(ans.size() > 1){
-                    temp_string.append(ans.pop());
+    public String removeOuterParentheses(String s) {
+        char[] result = new char[s.length()];
+        int index = 0;
+        int count = 0;
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                if(count > 0){
+                    result[index++] = c;
                 }
-                s_ans.append(temp_string.reverse());
-                int temp2 = ans.pop();
-                left_count = 0;
-                right_count = 0;
+                count++;
+            }else{
+            count--;
+                if(count > 0){
+                    result[index++] = c;
+                }
+
             }
         }
-        return s_ans.reverse().toString();
+        return new String(result,0,index);
     }
 }
